e11cc6fe81bbbd216d37a76b107164d48fc53bc9
Joel Nothman <joel.nothman@gmail.com>
2012-03-13 14:41:38 +1100

    Add github url to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 
 setup(name='datatemplate',
       version='0.1',
-      description='Renders various data sources via django templates',
+      description='Renders various data sources via Django templates',
       author='Joel Nothman',
       author_email='joel.nothman@gmail.com',
-###      url='',
+      url='https://github.com/jnothman/datatemplate',
       packages=['datatemplate', 'datatemplate.template_libs', 'datatemplate.load_libs'],
       scripts=['scripts/datatemplate'],
       install_requires=['django'],
